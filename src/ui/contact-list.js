61509b18bb4e9f64b0d9c13cccba1dfc1c2581e7
Brandon Thomas <lucky7id@gmail.com>
2016-07-21 14:05:42 -0700

    nesting working

--- src/ui/contact-list.js
+++ src/ui/contact-list.js
@@ -2,9 +2,10 @@ import Fetcher from '../domain/fetcher';
 
 // storing templates in simple functions allows for easy versioning and a/b testing
 const contactListTemplate = () => {
-    return `<div>lol</div>`
+    return `<div class="row" ng-repeat="item in $ctrl.items"><contact data="item"></contact></div>`
 }
 
+//extend the fetcher with view specifc routes, that way everything for this view is contained
 class ContactListFetcher extends Fetcher {
     constructor(http, q, listService) {
         super(http, q);
@@ -27,7 +28,7 @@ class ContactListFetcher extends Fetcher {
 class ContactList {
     constructor($http, $q) {
         this.fetcher = new ContactListFetcher($http, $q)
-
+        this.items = [1,2,3,4,5,6]
         console.log(this.fetcher);
     }
 }
@@ -38,5 +39,13 @@ const config = {
     template: contactListTemplate(),
     controller: ContactList
 }
+
+// once I have the contact component
+// const innerComponentConfig = {
+//     template: `<span>{{data}}</span>`,
+//     bindings: {
+//         data: '='
+//     }
+// }
 
 export default config;
